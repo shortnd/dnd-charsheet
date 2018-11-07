1e03306ba7078014805c286ce633d8cc53cb1b38
shortnd <ococncol@gmail.com>
2018-11-07 10:39:14 -0500

    started project

---
+++ database/factories/CharacterFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Character::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2018_11_07_152955_create_characters_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCharactersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('characters', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->string('name');
+            $table->string('class');
+            $table->string('subclass');
+            $table->unsignedInteger('level');
+            $table->string('race');
+            $table->string('subrace');
+            $table->boolean('public')->default('false');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('characters');
+    }
+}
